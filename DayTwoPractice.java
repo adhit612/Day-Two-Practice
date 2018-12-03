4a2462f6fe854be3b109278ecc0528ae96cbc8f6
adhit612 <adhit612@gmail.com>
2018-12-02 20:42:53 -0500

    Finished first part of practice
    Converting int to ArrayList

--- DayTwoPractice.java
+++ DayTwoPractice.java
@@ -1,9 +1,27 @@
+import java.util.ArrayList;
+
 public class DayTwoPractice{
 
+	public static ArrayList <Integer> convertToArrayList(int x){
+		ArrayList <Integer> list = new ArrayList<>();
 
+		while(x > 0){
+		list.add(x%10);
+		x /= 10;
+		}
+
+		ArrayList <Integer> listB = new ArrayList<>();
+
+		for(int i = list.size() - 1; i >= 0; i --){
+			listB.add(list.get(i));
+		}
+		return listB;
+		}
 
 
 	public static void main (String [] args){
+
+		System.out.print(convertToArrayList(895));
 
 
 

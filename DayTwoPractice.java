a96be320132a11cd6ad7d3551512b9e09744f5c9
adhit612 <adhit612@gmail.com>
2018-12-02 21:33:09 -0500

    Finished both parts of the program
    Finished day two practice on google classroom

--- DayTwoPractice.java
+++ DayTwoPractice.java
@@ -6,8 +6,8 @@ public class DayTwoPractice{
 		ArrayList <Integer> list = new ArrayList<>();
 
 		while(x > 0){
-		list.add(x%10);
-		x /= 10;
+			list.add(x%10);
+			x /= 10;
 		}
 
 		ArrayList <Integer> listB = new ArrayList<>();
@@ -15,13 +15,47 @@ public class DayTwoPractice{
 		for(int i = list.size() - 1; i >= 0; i --){
 			listB.add(list.get(i));
 		}
-		return listB;
+			return listB;
 		}
+
+	public static int convertToInt(ArrayList <Integer> list){
+		int x = 1;
+		int y = 0;
+
+		for(int i = list.size()-1; i >= 0; i --){
+			 y += list.get(i) * x;
+			 x *= 10;
+		}
+
+		return y;
+	}
+
 
 
 	public static void main (String [] args){
 
-		System.out.print(convertToArrayList(895));
+	System.out.println(convertToArrayList(8950));
+
+	ArrayList <Integer> list = new ArrayList<>();
+
+	list.add(8);
+
+	list.add(9);
+
+	list.add(5);
+
+	list.add(0);
+
+	System.out.println(convertToInt(list));
+
+
+
+
+
+
+
+
+
 
 
 
